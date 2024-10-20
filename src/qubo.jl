a588f61a22796aa402a87a920159af03a8534af6
Pedro Ripper <pedros.ripper@globo.com>
2024-10-20 15:46:45 -0300

    Refactor

--- src/qubo.jl
+++ src/qubo.jl
@@ -125,97 +125,9 @@ function _qubo_sample(rbm::RBMClassifier, model)
     h_sampled = zeros(Float64, num_hidden_nodes(rbm))
     total_samples = result_count(model)
     for i in 1:total_samples
-        v_sampled .+= vcat(value.(model[:vis], result = i))
+        v_sampled .+= value.(model[:vis], result = i)
         h_sampled .+= value.(model[:hid], result = i)
         label_sampled .+= value.(model[:label], result = i)
     end
     return v_sampled ./ total_samples, h_sampled ./ total_samples, label_sampled ./ total_samples
-end
-
-function persistent_qubo!(
-    rbm::AbstractRBM,
-    model,
-    x,
-    epoch::Int,
-    mini_batches::Vector{UnitRange{Int}};
-    learning_rate::Float64 = 0.1,
-    evaluation_function::Function,
-    metrics::Any,
-)
-    total_t_sample, total_t_qs, total_t_update = 0.0, 0.0, 0.0
-    for mini_batch in mini_batches
-        t_qs = time()
-        v_model, h_model = _qubo_sample(rbm, model) # v~, h~
-        total_t_qs += time() - t_qs
-        for sample in x[mini_batch]
-            t_sample = time()
-            v_data = sample # training visible
-            h_data = conditional_prob_h(rbm, v_data) # hidden from training visible
-            total_t_sample += time() - t_sample
-
-            # Update hyperparameter
-            t_update = time()
-            update_rbm!(
-                rbm,
-                v_data,
-                h_data,
-                v_model,
-                h_model,
-                (learning_rate / length(mini_batch)),
-            )
-            total_t_update += time() - t_update
-
-            evaluation_function(rbm, sample, metrics, epoch)
-        end
-        t_update = time()
-        _update_qubo_model!(model, rbm)
-        total_t_update += time() - t_update
-    end
-    return total_t_sample, total_t_qs, total_t_update
-end
-
-function persistent_qubo!(
-    rbm::RBMClassifier,
-    model,
-    x,
-    label,
-    epoch::Int,
-    mini_batches::Vector{UnitRange{Int}};
-    learning_rate::Float64 = 0.1,
-    label_learning_rate::Float64 = 0.1,
-    evaluation_function::Function,
-    metrics::Any,
-)
-    total_t_sample, total_t_qs, total_t_update = 0.0, 0.0, 0.0
-    for mini_batch in mini_batches
-        t_qs = time()
-        v_model, h_model, label_model = _qubo_sample(rbm, model) # v~, h~
-        total_t_qs += time() - t_qs
-        for sample_i in mini_batch
-            t_sample = time()
-            v_data = x[sample_i]
-            label_data = label[sample_i]
-            h_data = conditional_prob_h(rbm, v_data, label_data) # hidden from training visible
-            total_t_sample += time() - t_sample
-
-            # Update hyperparameter
-            t_update = time()
-            update_rbm!(
-                rbm,
-                v_data,
-                h_data,
-                label_data,
-                v_model,
-                h_model,
-                label_model,
-                (learning_rate / length(mini_batch)),
-                (label_learning_rate / length(mini_batch)),
-            )
-            total_t_update += time() - t_update
-        end
-        t_update = time()
-        _update_qubo_model!(model, rbm)
-        total_t_update += time() - t_update
-    end
-    return total_t_sample, total_t_qs, total_t_update
 end
