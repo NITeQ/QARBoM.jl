dc03913df794853f993b07ce2d3d774551b7686e
pedroripper <pedros.ripper@gmail.com>
2025-05-10 12:42:12 -0300

    Fixes

--- src/training/train_fast_pcd.jl
+++ src/training/train_fast_pcd.jl
@@ -215,9 +215,9 @@ function train!(
         total_t_update += t_update
 
         if !isnothing(x_test_dataset)
-            evaluate(rbm, metrics, x_test_dataset, metrics_dict, epoch)
+            evaluate(rbm, metrics, x_test_dataset, metrics_dict)
         else
-            evaluate(rbm, metrics, x_train, metrics_dict, epoch)
+            evaluate(rbm, metrics, x_train, metrics_dict)
         end
 
         if _diverged(metrics_dict, epoch, stopping_metric)
@@ -353,9 +353,9 @@ function train!(
         total_t_update += t_update
 
         if !isnothing(x_test_dataset)
-            evaluate(rbm, metrics, x_test_dataset, y_test_dataset, metrics_dict, epoch)
+            evaluate(rbm, metrics, x_test_dataset, y_test_dataset, metrics_dict)
         else
-            evaluate(rbm, metrics, x_train, label_train, metrics_dict, epoch)
+            evaluate(rbm, metrics, x_train, label_train, metrics_dict)
         end
 
         if _diverged(metrics_dict, epoch, stopping_metric)
