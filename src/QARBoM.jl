9267ea60f85bbaedee9582ea1985dca4e012409d
pedroripper <pedros.ripper@gmail.com>
2025-08-07 18:17:41 -0300

    Minor fix

--- src/QARBoM.jl
+++ src/QARBoM.jl
@@ -15,9 +15,7 @@ abstract type PCD <: TrainingMethod end
 abstract type CD <: TrainingMethod end
 abstract type FastPCD <: TrainingMethod end
 
-abstract type AbstractDBN end
 abstract type AbstractRBM end
-abstract type DBNLayer end
 
 export RBM, RBMClassifier, GRBM, GRBMClassifier
 export QSampling, PCD, CD, FastPCD
@@ -28,12 +26,9 @@ include("evaluation.jl")
 include("gibbs.jl")
 include("qubo.jl")
 include("fantasy_data.jl")
-include("dbn.jl")
 include("training/train_cd.jl")
 include("training/train_pcd.jl")
 include("training/train_fast_pcd.jl")
 include("training/train_quantum_pcd.jl")
-include("training/pretrain_dbn.jl")
-include("training/fine_tune_dbn.jl")
 
 end # module QARBoM
